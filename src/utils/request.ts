7e2cab865b9cf4bd2e13464e7cd31f539ca0b3f4
THLIVSQAZ <zgmzzzz18@gmail.com>
2020-06-19 12:52:47 +0800

    精简错误提示

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -10,8 +10,10 @@ const codeMessage = {
   201: '新建或修改数据成功。',
   202: '一个请求已经进入后台排队（异步任务）。',
   204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
+  // 400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  400: '',
+  // 401: '用户没有权限（令牌、用户名、密码错误）。',
+  401: '请先登陆',
   403: '用户得到授权，但是访问是被禁止的。',
   404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
   406: '请求的格式不可得。',
@@ -30,10 +32,11 @@ const errorHandler = (error: { response: Response }): Response => {
   const { response } = error;
   if (response && response.status) {
     const errorText = codeMessage[response.status] || response.statusText;
-    const { status, url } = response;
+    // const { status, url } = response;
 
     notification.error({
-      message: `请求错误 ${status}: ${url}`,
+      // message: `请求错误 ${status}: ${url}`,
+      message:  `请求错误`,
       description: errorText,
     });
   } else if (!response) {
